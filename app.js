3adcb4869cea9c012924a15ee65489a8b09f3e83
Silva <GabrielSilva@gmail.com>
2021-11-10 11:02:31 -0300

    Primeira Versão Exercício 6

---
+++ app.js
@@ -0,0 +1,98 @@
+"use strict";
+/* a) Crie uma classe que retorne a bio do id passado
+ b) Crie uma classe que retorne o name do id passado
+ c) Crie uma classe que apague um item da lista a partir de um id passado
+ d) Crie uma classe que altere a bio ou o name a partir de um id passado
+ e) Demonstre todos os métodos com o paradigma funcional e com o imperativo
+ Atenção: A escolha de fazer 1 classe ou N classes é de cada pessoa*/
+class Person {
+    constructor(List) {
+        this.data = List;
+    }
+    getBioById(Id) {
+        for (const value of this.data) {
+            if (value.id === Id)
+                return value.bio;
+        }
+        return "Id Inválido";
+    }
+    getNameById(Id) {
+        for (const value of this.data) {
+            if (value.id === Id)
+                return value.name;
+        }
+        return "Id Inválido";
+    }
+    deleteElementById(Id) {
+        this.data.forEach((value) => {
+            if (value.id === Id)
+                this.data.splice(this.data.indexOf(value), 1);
+        });
+    }
+    changeElementById(Id, change, newString) {
+        change = change.toLowerCase();
+        this.data.forEach((value) => {
+            if (value.id === Id) {
+                if (change === "bio")
+                    value.bio = newString;
+                else if (change === "name")
+                    value.name = newString;
+                else
+                    console.log(`Opção de mudança ${change} inválida`);
+            }
+        });
+    }
+    getBioByIdF(Id) {
+        var _a;
+        return (_a = this.data.find((value) => value.id !== Id)) === null || _a === void 0 ? void 0 : _a.bio;
+    }
+    getNameByIdF(Id) {
+        var _a;
+        return (_a = this.data.find((value) => value.id !== Id)) === null || _a === void 0 ? void 0 : _a.name;
+    }
+    deleteElementByIdF(Id) {
+        return this.data.filter((value) => value.id !== Id);
+    }
+    changeNameByIdF(Id, newString) {
+        return this.data.map((value) => { if (value.id === Id)
+            value.name = newString; });
+    }
+    changeBioByIdF(Id, newString) {
+        this.data.map((value) => { if (value.id === Id)
+            value.bio = newString; });
+    }
+    changeElementByIdF(Id, change, newString) {
+        if (change.toLowerCase() === "bio")
+            return this.changeBioByIdF(Id, newString);
+        else if (change.toLowerCase() === "name")
+            return this.changeNameByIdF(Id, newString);
+    }
+}
+const lista = [
+    { "id": 1, "name": "Ada Lovelace", "bio": "Ada Lovelace, foi uma matemática e escritora inglesa reconhecida por ter escrito o primeiro algoritmo para ser processado por uma máquina" },
+    { "id": 2, "name": "Alan Turing", "bio": "Alan Turing foi um matemático, cientista da computação, lógico, criptoanalista, filósofo e biólogo teórico britânico, ele é amplamente considerado o pai da ciência da computação teórica e da inteligência artificial." },
+    { "id": 3, "name": "Nikola Tesla", "bio": "Nikola Tesla foi um inventor, engenheiro eletrotécnico e engenheiro mecânico sérvio, mais conhecido por suas contribuições ao projeto do moderno sistema de fornecimento de eletricidade em corrente alternada." },
+    { "id": 4, "name": "Nicolau Copérnico", "bio": "Nicolau Copérnico foi um astrônomo e matemático polonês que desenvolveu a teoria heliocêntrica do Sistema Solar." }
+];
+let p1 = new Person(lista);
+//Exemplos Imperativos
+console.log(p1.getBioById(1));
+console.log(p1.data[0]);
+console.log(p1.getNameById(1));
+console.log(p1.data[0]);
+p1.changeElementById(1, "Name", "Fernando");
+console.log(p1.data[0]);
+p1.changeElementById(1, "BIO", "O melhor padeiro da zona sul do rio");
+console.log(p1.data[0]);
+p1.deleteElementById(1);
+console.log(p1.data[0]);
+//Exemplos Funcionais
+console.log(p1.getBioByIdF(0));
+console.log(p1.data[0]);
+console.log(p1.getNameByIdF(1));
+console.log(p1.data[0]);
+p1.changeElementByIdF(2, "Name", "Fernando");
+console.log(p1.data[0]);
+p1.changeElementByIdF(2, "BIO", "O melhor padeiro da zona sul do rio");
+console.log(p1.data[0]);
+console.log(p1.deleteElementByIdF(2));
--- app.ts
+++ app.ts
@@ -5,20 +5,121 @@
  e) Demonstre todos os métodos com o paradigma funcional e com o imperativo
  Atenção: A escolha de fazer 1 classe ou N classes é de cada pessoa*/
 
-class Person{
-    public id: number;
-    public name: string;
-    public bio: string;
+type Info = {
+    id: number;
+    name: string;
+    bio: string;
+}
 
-    constructor(i: number, nm: string, b: string){
-        this.id=i;
-        this.name=nm;
-        this.bio=b;
+
+class Person{
+    public data: Info[];
+
+    constructor(List: Info[]){
+        this.data=List;
+    }
+
+    public getBioById(Id: number): any{
+        for(const value of this.data){
+            if(value.id === Id) return value.bio;
+        }
+        return "Id Inválido";
+    }
+
+    public getNameById(Id: number): string{
+        for(const value of this.data){
+            if(value.id === Id) return value.name;
+        }
+        return "Id Inválido";
+    }
+
+    public deleteElementById(Id: number): void{
+        this.data.forEach((value: Info) => {
+            if(value.id === Id) this.data.splice(this.data.indexOf(value),1);
+        });
+    }
+
+    public changeElementById(Id: number, change: string, newString: string): void{
+        change=change.toLowerCase();
+        this.data.forEach((value: Info) => {
+            if(value.id === Id){
+                if(change==="bio") value.bio = newString;
+                else if(change==="name") value.name =newString;
+                else console.log(`Opção de mudança ${change} inválida` );
+            }
+        });
+    }
+
+    public getBioByIdF(Id: number): string{
+        return this.data.find((value) => value.id !== Id)?.bio as string;
+    }
+
+    public getNameByIdF(Id: number): string{
+        return this.data.find((value) => value.id !== Id)?.name as string;
+    }
+
+    public deleteElementByIdF(Id:number): Info[]{
+        return this.data.filter((value: Info) => value.id !== Id);
+    }
+
+    private changeNameByIdF(Id: number, newString: string): any{
+        return this.data.map((value: Info) => {if(value.id === Id)value.name = newString;});
+        
+    }
+
+    private changeBioByIdF(Id: number, newString: string): any{
+        this.data.map((value: Info) => {if(value.id === Id)value.bio = newString;});
+        
+    }
+    public changeElementByIdF(Id: number, change: string, newString: string): any{
+        if(change.toLowerCase()==="bio") return this.changeBioByIdF(Id, newString);
+        else if(change.toLowerCase() === "name") return this.changeNameByIdF(Id, newString);
     }
 }
 
-let lista: Person[] = [
-    new Person(1, "Ada Lovelace", "Ada Lovelace, foi uma matemática e escritora inglesa reconhecida por ter escrito o primeiro algoritmo para ser processado por uma máquina"),
-    new Person(2, "Alan Turing", "Alan Turing foi um matemático, cientista da computação, lógico, criptoanalista, filósofo e biólogo teórico britânico, ele é amplamente considerado o pai da ciência da computação teórica e da inteligência artificia"),
-    new Person(3, "Nikola Tesla", "Nikola Tesla foi um inventor, engenheiro eletrotécnico e engenheiro mecânico sérvio, mais conhecido por suas contribuições ao projeto do moderno sistema de fornecimento de eletricidade em corrente alternada."),
-    new Person(4, "Nicolau Copérnico", "Nicolau Copérnico foi um astrônomo e matemático polonês que desenvolveu a teoria heliocêntrica do Sistema Solar.")];
+
+const lista: Info[]  = [
+    {"id" : 1, "name": "Ada Lovelace", "bio" : "Ada Lovelace, foi uma matemática e escritora inglesa reconhecida por ter escrito o primeiro algoritmo para ser processado por uma máquina"},
+    {"id" : 2, "name": "Alan Turing", "bio" : "Alan Turing foi um matemático, cientista da computação, lógico, criptoanalista, filósofo e biólogo teórico britânico, ele é amplamente considerado o pai da ciência da computação teórica e da inteligência artificial."},
+    {"id" : 3, "name": "Nikola Tesla", "bio" : "Nikola Tesla foi um inventor, engenheiro eletrotécnico e engenheiro mecânico sérvio, mais conhecido por suas contribuições ao projeto do moderno sistema de fornecimento de eletricidade em corrente alternada."},
+    {"id" : 4, "name": "Nicolau Copérnico", "bio": "Nicolau Copérnico foi um astrônomo e matemático polonês que desenvolveu a teoria heliocêntrica do Sistema Solar."}];
+
+let p1 = new Person(lista);
+
+
+//Exemplos Imperativos
+
+console.log(p1.getBioById(1));
+console.log(p1.data[0]);
+
+console.log(p1.getNameById(1));
+console.log(p1.data[0]);
+
+p1.changeElementById(1, "Name", "Fernando");
+console.log(p1.data[0]);
+
+p1.changeElementById(1, "BIO", "O melhor padeiro da zona sul do rio");
+console.log(p1.data[0]);
+
+p1.deleteElementById(1);
+console.log(p1.data[0]);
+
+//Exemplos Funcionais
+
+console.log(p1.getBioByIdF(0));
+console.log(p1.data[0]);
+
+console.log(p1.getNameByIdF(1));
+console.log(p1.data[0]);
+
+p1.changeElementByIdF(2,"Name","Fernando");
+console.log(p1.data[0]);
+
+p1.changeElementByIdF(2,"BIO","O melhor padeiro da zona sul do rio");
+console.log(p1.data[0]);
+
+console.log(p1.deleteElementByIdF(2));
+
+
+
+
